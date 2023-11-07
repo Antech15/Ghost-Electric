f3e9b276a028dd514b153bfa66c6e0be3babd824
Andres Hinojosa <shakycrab@gmail.com>
2023-11-07 12:11:01 -0800

    Revert "Merge branch 'main' into PlayerControl-Alejandro"
    This reverts commit 2fad4bd247b2a01fd0e27cf05799751ea780c843, reversing
    changes made to 945db3644afd22f3da36edd100845caf9698a3b8.

--- Assets/PlayerControls.cs
+++ Assets/PlayerControls.cs
@@ -5,32 +5,13 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f; // Public to allow changing speed in Unity Inspector
-    public float jumpSpeed = 15f;
-    public Rigidbody2D rb;
-    public float buttonTime = 0.3f;
-    public float jumpAmount = 20;
-    float jumpTime;
-    bool jumping;
+
     // Update is called once per frame
     void Update()
     {
         // Input detection for 'WASD' keys
         float moveX = Input.GetAxisRaw("Horizontal"); // 'A' 'D' keys and left/right arrow keys
         float moveY = Input.GetAxisRaw("Vertical"); // 'W' 'S' keys and up/down arrow keys
-        if (Input.GetKeyDown(KeyCode.Space)) 
-        {
-            jumping = true;
-            jumpTime = 0;
-        }
-        if(jumping)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpAmount);
-            jumpTime += Time.deltaTime;
-        }
-        if(Input.GetKeyUp(KeyCode.Space) || jumpTime > buttonTime)
-        {
-            jumping = false;
-        }
 
         // Movement vector
         Vector2 move = new Vector2(moveX, moveY).normalized; // Normalized to maintain constant speed in all directions
