0a0993ac745f55821df66126206d753cdab86244
marti1468 <124644815+marti1468@users.noreply.github.com>
2023-11-14 10:25:23 -0800

    Garbo

--- Assets/Enemy/EnemyControl.cs
+++ Assets/Enemy/EnemyControl.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 
 public class EnemyControl : EnemyFSM
 {
+    public GameObject pointA;
+    public GameObject pointB;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private Transform currentPoint;
+
     public enum FSMState
     {
         None,
@@ -18,7 +24,7 @@ public class EnemyControl : EnemyFSM
     public FSMState curState;
 
     //Speed of the enemy
-    private float curSpeed;
+    public float curSpeed;
 
     //Whether the NPC is destroyed or not
     private bool bDead;
@@ -29,11 +35,16 @@ public class EnemyControl : EnemyFSM
 
     protected override void Initialize()
     {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        currentPoint = pointB.transform;
+        //anim.SetBool("isRunning", true);
         curState = FSMState.Patrol;
-        curSpeed = 150.0f;
+        curSpeed = 5.0f;
         bDead = false;
         //elapsedTime = 0.0f;
         health = 100;
+       
     }
 
     //Update each frame
@@ -57,23 +68,49 @@ public class EnemyControl : EnemyFSM
 
     protected void UpdatePatrolState()
     {
+        Debug.Log("In patrol state");
         distance = playerDistance();
-        if (distance < 25.0f && distance > 10.0f)
-            curState = FSMState.Chase;
-        else if (distance < 10.0f)
-            curState = FSMState.Attack;
-        else
+        Debug.Log("Dist is " + distance);
+        if (distance > 10.0f)
+        {
             curState = FSMState.Patrol;
+            Vector2 point = currentPoint.position - transform.position;
+            rb.velocity = new Vector2(curSpeed, 0);
+            if (transform.position.x == pointB.transform.position.x)
+            {
+                flip();
+                rb.velocity = new Vector2(curSpeed, 0);
+            }
+            else if(transform.position.x == pointA.transform.position.x)
+            {
+                flip();
+                rb.velocity = new Vector2(-curSpeed, 0);
+            }
 
+
+        }
+       // else if (distance < 10.0f && distance >= 5.0f)
+        //    curState = FSMState.Chase;
+        //else if (distance < 5.0f)
+         //   curState = FSMState.Attack;
+
+
+        //transform.position += transform.right * curSpeed * Time.deltaTime;
     }
 
     protected void UpdateChaseState()
-    { 
-    
+    {
+        distance = playerDistance();
+        if (distance > 10.0f)
+            curState = FSMState.Patrol;
+        else if (distance < 5.0f)
+            curState = FSMState.Chase;
+
+
     }
     protected void UpdateAttackState()
     {
-      
+        distance = playerDistance();
     }
     protected void UpdateDeadState()
     {
@@ -83,7 +120,17 @@ public class EnemyControl : EnemyFSM
 
     protected float playerDistance()
     {
-        float dist = Vector3.Distance(transform.position, player.transform.position);
+        float dist = Vector2.Distance(transform.position, player.transform.position);
         return dist;
+    }
+    protected void jump()
+    {
+
+    }
+
+    private void flip()
+    {
+        Vector3 localScale = transform.localScale;
+        localScale.x = -1;
     }
 }
