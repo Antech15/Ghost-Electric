e4f671d8c030be6f6301f0f92f4474d8fa43a252
Andres Hinojosa <shakycrab@gmail.com>
2023-11-30 01:10:35 -0800

    lol

--- Assets/Scenes/GameLevelJumping.cs
+++ Assets/Scenes/GameLevelJumping.cs
@@ -8,9 +8,15 @@ public class SceneJump2 : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision) // Updated for 2D collision
     {
         Debug.Log("2D Collision entered!");
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") && SceneManager.GetActiveScene().name != "Slums")
         {
             SceneManager.LoadScene("Slums"); // Scene you want to jump to name
         }
+        //if we are in Slums, jump to the next scene
+        else
+        {
+            SceneManager.LoadScene("Boss1"); // Scene you want to jump to name
+        }
+        
     }
 }
